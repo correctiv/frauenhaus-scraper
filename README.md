83d3b8c3ae7df31d82d1b7ebd97bb544f67f1d46
Max Donheiser <maxdonheiser@Maxs-MacBook-Pro.local>
2021-12-17 18:17:11 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frauenhaus Scraper
+
+Scraping data from [Zentrale Informationsstelle Autonomer Frauenhäuser](https://www.frauenhaus-suche.de/?jumpto=searchResults). 3x daily.
---
+++ scraper.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import ast
+import os
+from datetime import datetime
+import json
+
+def formatNewEntry(entry):
+
+	props = entry['properties']
+	props['geography'] = entry['geometry']
+	props.pop('uid')
+	props.pop('freePlacesType')
+	props['data'] = []
+	return props
+
+fpath = 'frauenhaus_suche.json'
+
+with open(fpath) as f:
+	data = json.loads(f.read())
+
+r = requests.get('https://www.frauenhaus-suche.de/')
+soup = BeautifulSoup(r.content, 'lxml')
+script = soup.find('div', class_='tx-ks-zif-frauenhaus').find_all('script')[1].string.replace('\n','').replace('\t','').replace(';','').split('addressesJson =  ')[1]
+d = ast.literal_eval(script)['features']
+
+existing = data.keys()
+timestamp = datetime.now().strftime("%d.%m.%Y, %H:%M")
+
+for entry in d:
+
+	uid = entry['properties']['uid']
+	freePlaces = entry['properties']['freePlacesType']
+
+	if uid not in existing:
+
+		props = formatNewEntry(entry)
+		data[uid] = props
+
+	data[uid]['data'].append({ 'timestamp': timestamp, 'freePlaces': freePlaces })
+
+with open(fpath, 'w') as f:
+	json.dump(data, f)
